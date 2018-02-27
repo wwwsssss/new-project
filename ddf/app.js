636a1b765e0d6a52c0e878e7c70fc95d898b860a
Your Name <you@example.com>
2018-02-27 22:58:34 +0800

    周二进度

--- ddf/app.js
+++ ddf/app.js
@@ -6,11 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var admin = require('./routes/admin');
 var users = require('./routes/users');
 
 var app = express();
-
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/1725");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -24,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/admin',admin);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- ddf/routes/index.js
+++ ddf/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
-
+var UserModel = require("../model/UserModel");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/admin',function(req,res){
+	res.render('admin');
+})
+router.get('/goods_add',function(req,res){
+	res.render('goods_add');
+})
+router.get('/control',function(req,res){
+	res.render('control');
+})
+router.post('/admin',function(req,res){	
+	var username = req.body.username;
+	var psw = req.body.password;	
+	var result = {
+		status : 1,
+		message : "登录成功"
+	}
+	
+	UserModel.find({username:username,psw:psw},function(err,docs){
+		if(!err&&docs.length>0){
+			console.log("登录成功");
+			res.send(result);
+		}else{
+			console.log("登录失败");
+			result.status = -1;
+			result.message = "登录失败";
+			res.send(result);	 		
+		}		
+	})	
+})
 module.exports = router;
