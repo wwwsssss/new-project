2e6c8552e7bf52320b32d5b0f792965ee9f3576b
Your Name <you@example.com>
2018-03-02 18:05:41 +0800

    周五进度

--- ddf/model/GoodsModel.js
+++ ddf/model/GoodsModel.js
@@ -2,9 +2,10 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;   
 var Goods = new Schema({    
     goods_name     : String,
-    price      : String,
-    img      : String,
-    detail      : String,
+    shop_price      : String,
+    goods_number      : String,
+    vertual_sales      : String,
+    goods_sn      : String,
     create_date      : { type: Date, default: Date.now }
 });
 var GoodsModel = mongoose.model('goods', Goods);
--- ddf/routes/index.js
+++ ddf/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var UserModel = require("../model/UserModel");
+var GoodsModel = require("../model/GoodsModel");
+var multiparty = require('multiparty')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -14,9 +16,7 @@ router.get('/goods_add',function(req,res){
 router.get('/control',function(req,res){
 	res.render('control');
 })
-router.get('/goods_list',function(req,res){
-	res.render('goods_list');
-})
+
 router.post('/admin',function(req,res){	
 	var username = req.body.username;
 	var psw = req.body.password;	
@@ -37,4 +37,38 @@ router.post('/admin',function(req,res){
 		}		
 	})	
 })
+
+router.post('/goods_list',function(req,res){
+	var Form = new multiparty.Form();
+	Form.parse(req,function(err,body,files){
+		var goods_name = body.goods_name[0];
+		var goods_sn = body.goods_sn[0];
+		var shop_price = body.shop_price[0];
+		var goods_number = body.goods_number[0];
+		var vertual_sales = body.vertual_sales[0];
+//		var imgName = files.img[0].path;
+//		imgName = imgName.substr(imgName.lastIndexOf("\\")+1);
+		var gm = new GoodsModel();
+		gm.goods_name = goods_name;
+		gm.shop_price = shop_price;
+		gm.goods_sn = goods_sn;
+		gm.goods_number = goods_number;
+		gm.vertual_sales = vertual_sales;
+//		gm.img = imgName;
+		gm.save(function(err){
+			if(!err){
+				res.send("商品保存成功");
+			}else{
+				res.send("商品保存失败");
+			}
+		})
+	})
+})
+router.get('/goods_list',function(req,res){
+	GoodsModel.find({},function(err,docs){
+		res.render("goods_list",{list:docs});
+	})
+	
+})
 module.exports = router;
+
